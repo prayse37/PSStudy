4fdf07aa0aa0a22e694c7bbdd08ac08c90925ad0
prayse37 <prayse37@gmail.com>
2025-04-20 00:34:03 -0700

    week3 updated

--- week3/1_Backtracking.cpp
+++ week3/1_Backtracking.cpp
@@ -5,16 +5,16 @@ vector<vector<int>> answer;
 
 void backtrack(int N, int sum, int start, vector<int>& path) {
     if (sum == 10) {
-        answer.push_back(path); // 정답 저장
+        answer.push_back(path);
         return;
     }
 
     for (int i = start; i <= N; i++) {
-        if (sum + i > 10) continue; // 가지치기: 10 넘으면 스킵
+        if (sum + i > 10) continue;
 
-        path.push_back(i);           // 선택
-        backtrack(N, sum + i, i + 1, path); // 다음 숫자로 이동 (중복 방지)
-        path.pop_back();             // 되돌리기 (백트래킹 핵심)
+        path.push_back(i);
+        backtrack(N, sum + i, i + 1, path);
+        path.pop_back();
     }
 }
 
